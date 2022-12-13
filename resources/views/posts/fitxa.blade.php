5a9e6021e2b909dc140702edcf87403670cc5ea8
igomis <igomis@cipfpbatoi.es>
2022-12-13 13:42:51 +0100

    First Branch

--- resources/views/posts/fitxa.blade.php
+++ resources/views/posts/fitxa.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.plantilla')
 @section('titulo', 'Fitxa Posts')
 @section('contenido')
-    <h1>Benvingut al blog d'Ignasi</h1>
+    <h1>Fitxa del post {{$id}}</h1>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('inici');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -27,5 +27,13 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('posts', function() {
+    return view('posts.llistat');
+})->name('posts_llistat');
+
+Route::get('posts/{id}', function($id) {
+    return view('posts.fitxa',compact('id'));
+})->where('id', "[0-9]+")->name('posts_fitxa');
 
 require __DIR__.'/auth.php';
